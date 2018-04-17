88efde36fac888eba9db576796bcf235f6c56128
inunotaisho26 <ebfairweather@gmail.com>
2018-04-17 16:52:49 -0400

    updating contact models

--- inunotaisho/models/contact.cs
+++ inunotaisho/models/contact.cs
@@ -13,10 +13,10 @@ namespace Inunotaisho.Models.Contact
         // external ID or key, which may be easier to reference (ex: 1,2,3 etc.)
         public string Id { get; set; }
 
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string email { get; set; }
-        public string subject { get; set; }
-        public string message { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
     }
 }
