9019575dac5c9a215e94b85b88d973c7069c8c70
inunotaisho26 <ebfairweather@gmail.com>
2018-04-17 22:26:32 -0400

    commenting out Pbkd2 for now

--- inunotaisho/lib/Program.cs
+++ inunotaisho/lib/Program.cs
@@ -19,23 +19,23 @@ namespace Inunotaisho
         {
             BuildWebHost(args).Run();
 
-            string password = UserSchema.Password;
+            //string password = UserSchema.Password;
 
-            // generate a 128-bit salt using a secure PRNG
-            byte[] salt = new byte[128 / 8];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+            //// generate a 128-bit salt using a secure PRNG
+            //byte[] salt = new byte[128 / 8];
+            //using (var rng = RandomNumberGenerator.Create())
+            //{
+            //    rng.GetBytes(salt);
+            //}
+            //Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
 
-            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 10000,
-                numBytesRequested: 256 / 8));
+            //// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            //string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                //password: password,
+                //salt: salt,
+                //prf: KeyDerivationPrf.HMACSHA1,
+                //iterationCount: 10000,
+                //numBytesRequested: 256 / 8));
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
