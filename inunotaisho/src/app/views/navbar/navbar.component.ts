21820cb0469f0d239e9bbfd23383edfbd246f97b
inunotaisho26 <ebfairweather@gmail.com>
2018-04-17 22:20:48 -0400

    adding httpclient for angular 5 and added logout

--- inunotaisho/src/app/views/navbar/navbar.component.ts
+++ inunotaisho/src/app/views/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import { NgIf } from '@angular/common';
-import { Http } from '@angular/http';
+import { HttpClient} from '@angular/common/http';
 import { Subscription } from 'rxjs/Subscription';
 
 
@@ -35,7 +35,7 @@ export function services(): any {
 export class Navbar implements OnDestroy {
     logoutSubscription:Subscription;
 
-    constructor(private http:Http, private authService: AuthService){
+    constructor(private http:HttpClient, private authService: AuthService){
 
     }
     
@@ -44,11 +44,7 @@ export class Navbar implements OnDestroy {
     }
 
     logout = () => {
-        this.logoutSubscription = this.http.get('/logout').subscribe((res) => {
-            if(res.status) {
-                this.authService.setIsLoggedIn(false);
-            }
-        })
+        this.authService.logout();
        
     }
     ngOnDestroy() {
