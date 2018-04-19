b492d2a0d3a3f159770c1f7f2d58b05e16e7ddda
inunotaisho <ebfairweather@gmail.com>
2018-04-18 23:40:07 -0400

    removing Cryptography and KeyDerivation

--- inunotaisho/lib/Program.cs
+++ inunotaisho/lib/Program.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Inunotaisho.Models.User;
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -18,24 +16,6 @@ namespace Inunotaisho
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-
-            //string password = UserSchema.Password;
-
-            //// generate a 128-bit salt using a secure PRNG
-            //byte[] salt = new byte[128 / 8];
-            //using (var rng = RandomNumberGenerator.Create())
-            //{
-            //    rng.GetBytes(salt);
-            //}
-            //Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
-
-            //// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-            //string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                //password: password,
-                //salt: salt,
-                //prf: KeyDerivationPrf.HMACSHA1,
-                //iterationCount: 10000,
-                //numBytesRequested: 256 / 8));
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
