6d975200306f4f7c65357573e0a34ece607fa335
inunotaisho26 <ebfairweather@gmail.com>
2018-04-19 19:47:15 -0400

    updating startup cs to initiate mongodb connection at launch

--- inunotaisho/lib/Startup.cs
+++ inunotaisho/lib/Startup.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Inunotaisho.Models.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
