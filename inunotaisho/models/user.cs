481cb82e7ad065ae624985dd600cd7082ddd4a72
inunotaisho26 <ebfairweather@gmail.com>
2018-04-19 19:36:02 -0400

    moving models to Models

--- inunotaisho/models/user.cs
+++ inunotaisho/models/user.cs
@@ -17,6 +17,6 @@ namespace Inunotaisho.Models.User
         public string Password { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        //public string LastName { get; set; }
     }
 }
