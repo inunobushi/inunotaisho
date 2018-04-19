bb4441c2a2f36a19ff07292bccb4b33aebae2602
inunotaisho <ebfairweather@gmail.com>
2018-04-18 23:39:48 -0400

    adding Cryptography and KeyDerivation

--- inunotaisho/lib/Controllers/RegistrationController.cs
+++ inunotaisho/lib/Controllers/RegistrationController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Inunotaisho.Models.User;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -32,6 +34,23 @@ namespace Inunotaisho.lib.Controllers
         [HttpPost]
         public void Post([FromBody]UserSchema RegData)
         {
+            string password = RegData.Password;
+
+            // generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
             var data = RegData;
         }
 
