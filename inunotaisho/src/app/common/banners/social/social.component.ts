9abe6ebb05f5e149f991bf5e21570f1e5f12b1b9
inunotaisho26 <ebfairweather@gmail.com>
2018-07-11 17:24:23 -0400

    ading social component

---
+++ inunotaisho/src/app/common/banners/social/social.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+    selector:'social-component',
+    templateUrl:'./social.component.html' //template: portImgComponent
+})
+
+export  class SocialBannerComponent {
+    constructor(){
+
+    }
+
+}
